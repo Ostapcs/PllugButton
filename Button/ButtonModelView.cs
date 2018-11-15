e759df43ef5281c3ef2c7539f5a604da2d307929
Ostap Kutyanskyy <ostapkutya@gmail.com>
2018-11-15 19:16:55 +0200

    init

---
+++ Button/ButtonModelView.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+//локалізація 
+namespace Button
+{
+    public class ButtonModelView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Model model { get; set; }
+        public ButtonModelView()
+        {
+            model = new Model();
+        }
+
+        public int CornRadiusValue
+        {
+            set
+            {
+                model.cornRadiusValue = value;
+                OnPropertyChanged("CornRadiusValue");
+
+            }
+            get
+            {
+                return model.cornRadiusValue;
+            }
+        }
+
+        public int FontSizeValue
+        {
+            set
+            {
+                model.fontSizeValue = value;
+                OnPropertyChanged("FontSizeValue");
+            }
+            get
+            {
+                return model.fontSizeValue;
+            }
+        }
+
+        public int BorderWidthValue
+        {
+            set
+            {
+                model.borderWidthValue = value;
+                OnPropertyChanged("BorderWidthValue");
+            }
+            get
+            {
+                return model.borderWidthValue;
+            }
+        }
+
+        public List<string> Colours
+        {
+            get 
+            {
+                return model.colours;
+            }
+            set
+            {
+                model.colours = value;
+                OnPropertyChanged("Colours");
+            }
+
+        }
+
+
+        public string SelectedBackgroundcolour
+        {
+            get
+            {
+                return model.backgroundcolour;
+            }
+            set
+            {
+                model.backgroundcolour = value;
+                OnPropertyChanged("SelectedBackgroundcolour");
+            }
+        }
+
+       
+        public string SelectedTextColour
+        {
+            get
+            {
+                return model.textcolour;
+            }
+            set
+            {
+                model.textcolour = value;
+                OnPropertyChanged("SelectedTextColour");
+            }
+        }
+
+        public string SelectedBorderColour
+        {
+            get
+            {
+                return model.bordercolour;
+            }
+            set
+            {
+                model.bordercolour = value;
+                OnPropertyChanged("SelectedBorderColour");
+            }
+        }
+
+
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Button/Model.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace Button
+{
+    public class Model
+    {
+        public List<string> colours { get; set; }
+        public int cornRadiusValue { get; set; }
+        public int fontSizeValue { get; set; }
+        public int borderWidthValue { get; set; }
+        public int colouSelectedIndex { get; set; }
+        public string backgroundcolour { get; set; }
+        public string textcolour { get; set; }
+        public string bordercolour { get; set; }
+
+        public Model()
+        {
+            fontSizeValue = 50;
+
+            backgroundcolour = "Black";
+            textcolour = "White";
+            bordercolour = "Yellow";
+
+            ColorTypeConverter converter = new ColorTypeConverter();
+            colours = new List<string>();
+
+            foreach (var field in typeof(Xamarin.Forms.Color).GetFields(BindingFlags.Static | BindingFlags.Public))
+            {
+                if (field != null && !String.IsNullOrEmpty(field.Name))
+                    colours.Add(field.Name);
+            }
+        }
+    }
+}
